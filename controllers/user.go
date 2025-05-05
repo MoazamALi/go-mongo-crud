44a9c598dd1a1a995366ca6ddd851ae5e3751b78
Moazam Ali <contactmoazam@gmail.com>
2025-05-05 13:56:01 +0500

    inital

--- controllers/user.go
+++ controllers/user.go
@@ -1,41 +1,104 @@
 package controllers
 
 import (
-	"enconding/json"
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 
-	"github.com/MoazamAli/go-mongo-crud/models"
+	"github.com/MoazamAli/go-mongo-crud.git/models"
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserController struct {
-	session *mgo.Session
+	session *mongo.Client
 }
 
-func NewUserController(s *mgo.Session) *UserController {
+func isValidObjectId(id string) bool {
+	_, err := primitive.ObjectIDFromHex(id)
+	return err == nil
+}
+func NewUserController(s *mongo.Client) *UserController {
 	return &UserController{s}
+}
+
+func (uc *UserController) GetUsers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cursor, err := uc.session.Database("go-mongo-crud").Collection("users").Find(ctx, bson.M{})
+	if err != nil {
+		log.Fatal("Find failed:", err)
+	}
+	var users []models.User
+	if err = cursor.All(ctx, &users); err != nil {
+		log.Fatal(err)
+	}
+	uj, err := json.Marshal(users)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(uj)
 }
 
 func (uc *UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	id := p.ByName("id")
-	if !bson.IsObjectIdHex(id) {
+	if !isValidObjectId(id) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
-	oid := bson.ObjectIdHex(id)
+	oid, _ := primitive.ObjectIDFromHex(id)
 	u := models.User{}
-
-	if err := uc.session.DB("go-mongo-crud").C("users").FindId(oid).One(&u); err != nil {
-		w.WriteHeader(404)
-		return
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	// var result bson.M
+	err := uc.session.Database("go-mongo-crud").Collection("users").FindOne(ctx, bson.M{"_id": oid}).Decode(&u)
+	if err != nil {
+		log.Fatal("Find failed:", err)
 	}
+	// fmt.Println("Found document:", result)
+
+	// if err := uc.session.Database("go-mongo-crud").Collection("users").FindOne(ctx,).One(&u); err != nil {
+	// 	w.WriteHeader(404)
+	// 	return
+	// }
 	uj, err := json.Marshal(u)
 	if err != nil {
 		fmt.Println(err)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(uj)
+}
+
+func (uc *UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	u := models.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = primitive.NewObjectID()
+	uc.session.Database("go-mongo-crud").Collection("users").InsertOne(context.TODO(), u)
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(uj)
+}
+
+func (uc *UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	if !isValidObjectId(id) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	oid, _ := primitive.ObjectIDFromHex(id)
+	if _, err := uc.session.Database("go-mongo-crud").Collection("users").DeleteOne(context.TODO(), bson.M{"_id": oid}); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- main.go
+++ main.go
@@ -1,26 +1,51 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
-	"github.com/MoazamAli/go-mongo-crud/controllers"
+	"github.com/MoazamAli/go-mongo-crud.git/controllers"
 	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	r := httprouter.New()
 	uc := controllers.NewUserController(getSession())
-	r.GET("/users/:id", uc.GetUser)
+	r.GET("/user/:id", uc.GetUser)
+	r.GET("/users", uc.GetUsers)
 	r.POST("/users", uc.CreateUser)
-	r.DELETE("/users/:id", uc.DeleteUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
 	http.ListenAndServe(":8080", r)
 }
 
-func getSession() *mgo.Session {
-	s, err := mgo.Dial("mongodb://localhost:27017")
+func getSession() *mongo.Client {
+
+	// Set client options
+	clientOpts := options.Client().ApplyURI("mongodb://localhost:27017")
+
+	// Connect to MongoDB
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, clientOpts)
 	if err != nil {
-		panic(err)
+		log.Fatal("Connection error:", err)
 	}
-	return s
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("MongoDB not reachable:", err)
+	}
+	fmt.Println("Connected to MongoDB!")
+	return client
+
+	// s, err := mgo.Dial("mongodb://127.0.0.1:27017")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// return s
 }
